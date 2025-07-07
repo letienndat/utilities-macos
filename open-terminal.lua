8776bc5666646c35b10eed320340e61e6df39246
Le Tien Dat <dat.lt@extremevn.com.vn>
2025-07-07 14:12:33 +0700

    add source

---
+++ open-terminal.lua
@@ -0,0 +1,40 @@
+--============================================================================
+--=============OPEN CURRENT FOLDER WITH TERMINAL -- [CMD+SHIFT+T]=============
+--============================================================================
+
+-- Get the path of the folder currently open in Finder
+local function getCurrentFolderInFinder()
+    local _, output = hs.osascript.applescript([[
+        tell application "Finder"
+            try
+                set thePath to (POSIX path of (target of front window as alias))
+                return thePath
+            on error
+                return ""
+            end try
+        end tell
+    ]])
+    return output
+end
+
+-- Open Terminal.app in this path
+local function openTerminalAt(path)
+    if path ~= "" then
+        local script = [[
+            tell application "Terminal"
+                if not (exists window 1) then reopen
+                do script "cd ']] .. path .. [['" in front window
+                activate
+            end tell
+        ]]
+        hs.osascript.applescript(script)
+    else
+        hs.alert("There are no open folders in Finder")
+    end
+end
+
+-- Assign hotkey Cmd + Shift + T
+hs.hotkey.bind({"cmd", "shift"}, "T", function()
+    local folder = getCurrentFolderInFinder()
+    openTerminalAt(folder)
+end)
