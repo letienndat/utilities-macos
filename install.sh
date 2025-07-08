bc940844b50c888378c8735be1f4d627cd16dbd8
Le Tien Dat <attn110601@gmail.com>
2025-07-08 09:15:52 +0700

    remove bin, update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+set -e
+
 REAL_HOME=$(dscl . -read /Users/$(whoami) NFSHomeDirectory | awk '{print $2}')
 CONFIG_DIR="$REAL_HOME/.hammerspoon"
-REPO_DIR="/opt/homebrew/Cellar/utilities-macos/1.0.13"
+REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "Copying configs to: $CONFIG_DIR"
 mkdir -p "$CONFIG_DIR"
+
+if [ ! -d "$REPO_DIR" ]; then
+    echo "Repo folder not found: $REPO_DIR"
+    exit 1
+fi
 
 shopt -s dotglob nullglob
 cp -r "$REPO_DIR"/* "$CONFIG_DIR"/
 shopt -u dotglob nullglob
 
-echo "✅ Installed to $CONFIG_DIR"
+echo "Installed to $CONFIG_DIR"
