8a9fd344975d72ef56d795b749ca57ecff438101
Le Tien Dat <attn110601@gmail.com>
2025-07-08 08:13:03 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 CONFIG_DIR="$HOME/.hammerspoon"
 REPO_DIR="$(pwd)"
 
-mkdir -p "$CONFIG_DIR"
+# mkdir -p "$CONFIG_DIR"
 
-shopt -s dotglob nullglob
-for item in "$REPO_DIR"/* "$REPO_DIR"/.*; do
-    [[ "$(basename "$item")" == "." || "$(basename "$item")" == ".." ]] && continue
-    cp -r "$item" "$CONFIG_DIR"/
-done
-shopt -u dotglob nullglob
+# shopt -s dotglob nullglob
+# for item in "$REPO_DIR"/* "$REPO_DIR"/.*; do
+#     [[ "$(basename "$item")" == "." || "$(basename "$item")" == ".." ]] && continue
+#     cp -r "$item" "$CONFIG_DIR"/
+# done
+# shopt -u dotglob nullglob
